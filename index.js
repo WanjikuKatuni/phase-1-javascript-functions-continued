b2bb0b6c47750e763b459b6178c557a8da4491ac
wanjiku <annritawanjiku77@gmail.com>
2022-08-18 16:41:24 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+// razzle();
+
+// function razzle(lawyer="BILLY", TARGET="EM"){
+//     console.log(`YOU! ${lawyer} HAVE BEEN RAZZLED AT ${TARGET}`);
+// }
+
+// razzle('ann','gikomba');
+
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+saturdayFun();
+
+function mondayWork(activity="go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+
+mondayWork();
+
+function wrapAdjective(style='*'){
+    return function(adjective='special'){
+        return `You are ${style}${adjective}${style}!`
+    }
+}
+wrapAdjective()();
